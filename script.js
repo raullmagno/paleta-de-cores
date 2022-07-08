d175f7025cf5f97ba500e166bdfb5502f2fe7578
Raul Magno <raulmagno23@gmail.com>
2022-07-08 18:33:02 -0400

    Troquei as cores e adicioneilinhas de codigos para cada uma delas

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const mainDiv = document.getElementById('tabela-div');
 const primeiraCor = document.querySelector('.black');
-const segundaCor = document.querySelector('.red');
-const terceiraCor = document.querySelector('.orange');
-const quartaCor = document.querySelector('.yellow');
+const segundaCor = document.querySelector('.violet');
+const terceiraCor = document.querySelector('.pink');
+const quartaCor = document.querySelector('.gold');
 const colorPalette = document.querySelector('color');
 
 // Cria tabela com o tamanho de 5x5
@@ -34,3 +34,55 @@ window.onload = () => {
     });
   }
 };
+
+primeiraCor.addEventListener('click', () => {
+  for (let i = 0; i < colorPalette.length; i += 1) {
+    colorPalette[i].classList.remove('selected');
+  }
+  primeiraCor.classList.add('selected');
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', () => {
+      pixels[i].style.backgroundColor = 'rgb(0, 0, 0)';
+    });
+  }
+});
+
+segundaCor.addEventListener('click', () => {
+  for (let i = 0; i < colorPalette.length; i += 1) {
+    colorPalette[i].classList.remove('selected');
+  }
+  segundaCor.classList.add('selected');
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', () => {
+      pixels[i].style.backgroundColor = 'rgb(148,0,211)';
+    });
+  }
+});
+
+terceiraCor.addEventListener('click', () => {
+  for (let i = 0; i < colorPalette.length; i += 1) {
+    colorPalette[i].classList.remove('selected');
+  }
+  terceiraCor.classList.add('selected');
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', () => {
+      pixels[i].style.backgroundColor = 'rgb(255,20,147)';
+    });
+  }
+});
+
+quartaCor.addEventListener('click', () => {
+  for (let i = 0; i < colorPalette.length; i += 1) {
+    colorPalette[i].classList.remove('selected');
+  }
+  quartaCor.classList.add('selected');
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', () => {
+      pixels[i].style.backgroundColor = 'rgb(255,215,0)';
+    });
+  }
+});
